333d0c0add3f119e1ae8a4b77655b39091d55cd0
Crztbl <cristobal.alvareza@usm.cl>
2020-11-21 17:47:53 -0300

    Si

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className='titular'>bienvenido a The Gallery</h1>
+        <h1 className='titular'>Bienvenido a The Gallery</h1>
         <button className='boton_creador'>Acceder Creador</button>
         <button ClassName='boton_invitado'>Acceder Invitado</button>
       
