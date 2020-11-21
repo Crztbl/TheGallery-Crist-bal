9a485b3d081d8211662c7c92b8c449833d37279a
NoramMyers <72178964+NoramMyers@users.noreply.github.com>
2020-11-21 11:36:14 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='titular'>bienvenido a The Gallery</h1>
+        <button className='boton_inicial'>Acceder</button>
+      
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
